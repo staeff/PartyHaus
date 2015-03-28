0934a8cea438408518c40bb2fcf21133d0a5ab49
Stephan Klinger <staeff@arcor.de>
2015-03-28 13:54:21 +0100

    Map on index page

--- web/index.php
+++ web/index.php
@@ -1,22 +1,38 @@
-<?php
+<html>
+<head>
+  <title>Find your party haus in Berlin!</title>
+  <meta charset="utf-8" />
+  <!-- CSS -->
+  <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+  <style>
+  body {
+    padding: 0;
+    margin: 0;
+  }
+  html, body{
+    height: 100%;
+  }
+  </style>
+  <!-- JS -->
+  <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+</head>
+<body>
+  <!-- div-Element für die Karte einrichten -->
+  <div id="map" style="width: 100%; height: 100%"></div>
+  <script>
+  var map = L.map('map').setView([52.52,13.384], 12); //Grundkartenelement erzeugen
+  // colored map
+  L.tileLayer('http://a.tiles.mapbox.com/v3/examples.map-i87786ca/{z}/{x}/{y}.png').addTo(map);
+  // grayscale map
+  // L.tileLayer('http://a.tiles.mapbox.com/v3/examples.map-20v6611k/{z}/{x}/{y}.png').addTo(map);
+  var renate = L.marker([52.49744, 13.46531]).addTo(map);
+  renate.bindPopup("<b>Zur wilde Renate</b><br><a href='http://www.cluelist.com/de-de/berlin-salon_zur_wilden_renate'>Go to article about Zur Wilden Renate</a>.").openPopup();
 
-require('../vendor/autoload.php');
-
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return 'Hello Partyhaus';
-});
-
-$app->run();
-
-?>
+  // standalone popup
+  //var popup = L.popup()
+  //.setLatLng([52.49744, 13.46531])
+  //.setContent("Zur wilde Renate")
+  //.openOn(map);
+  </script>
+</body>
+</html>
